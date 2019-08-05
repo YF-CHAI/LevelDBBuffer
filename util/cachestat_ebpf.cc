0f4f4b207a2f23cd0c6ace4065e1de9b6244405d
YF-CHAI <chai-leon@qq.com>
2019-08-05 14:17:13 +0800

    add bcc

--- util/cachestat_ebpf.cc
+++ util/cachestat_ebpf.cc
@@ -35,7 +35,7 @@ leveldb::cache_info leveldb::Cachestat_eBPF::get_cache_info()
 {
     struct cache_info info;
     auto cache_hash_table = bpf_.get_hash_table<struct key_t, uint64_t>("counts");
-    std::cout<< "get into leveldb::cache_info leveldb::Cachestat_eBPF::get_cache_info()"<<std::endl;
+    std::cout<< "Cachestat_eBPF::get_cache_info() table_size:"<<cache_hash_table.get_table_offline().size()<<std::endl;
     for (auto it: cache_hash_table.get_table_offline()) {
         std::string pid_name = it.first.comm;
         std::cout <<"The pid name: "<< pid_name <<std::endl;//cyf add
