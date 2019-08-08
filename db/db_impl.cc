2166f0402edd14199cede79f858b7cabb7f34f61
YF-CHAI <chai-leon@qq.com>
2019-08-08 13:01:13 +0800

    test thread probe and stat

--- db/db_impl.cc
+++ db/db_impl.cc
@@ -1351,8 +1351,8 @@ void DBImpl::ProbeKernelFunction()
     std::cout <<"current tid: " << tid << "Stmp[0].partial_stats.bytes_written"<<Stmp[0].partial_stats.bytes_written<< std::endl;
     std::cout <<"current tid: " << tid <<"stats_[0].partial_stats.bytes_written"<<stats_[0].partial_stats.bytes_written<< std::endl;
 
-    //for(int i = 0; i < config::kNumLevels; i++)
-        //Stmp[i].SubstractBy(stats_[i]);
+    for(int i = 0; i < config::kNumLevels; i++)
+        Stmp[i].SubstractBy(stats_[i]);
     std::cout << "SubstractBy Stmp[0].partial_stats.bytes_written"<<Stmp[0].partial_stats.bytes_written<< std::endl;
 
     if (false) {
